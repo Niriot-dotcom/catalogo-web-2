7a2c51d7dc20c9f45366ecbfe528b1ffdc65ac24
Paty Lopez <lopezpatyyarely@gmail.com>
2024-07-19 02:11:15 -0600

    finish DiferenciasDeCobertores communication.

---
+++ catalogo-web/src/lib/components/HorizontalScroll.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+
+  let isUp = false;
+
+  function animate() {
+    isUp = !isUp;
+
+    setTimeout(
+      () => {
+        animate();
+      },
+      isUp ? 400 : 700,
+    );
+  }
+
+  onMount(() => {
+    animate();
+  });
+</script>
+
+<div class="w-full h-full" style="transition: all 0.5 linear;">
+  <!-- horizontal -->
+  <div class="w-full h-full">
+    <img
+      src="../images/icons/hand-scroll-black.svg"
+      loading="eager"
+      alt=""
+      class="w-full h-full"
+      style={isUp
+        ? "transition: transform 0.2s ease-out; transform: translateX(-50px);"
+        : "transition: transform 0.2s ease-out; transform: translateX(0);"}
+    />
+  </div>
+</div>
--- catalogo-web/src/lib/components/resources/ArrowLeft.svelte
+++ catalogo-web/src/lib/components/resources/ArrowLeft.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svg
-  class="w-5 h-5"
+  class="w-8 h-8"
   version="1.0"
   xmlns="http://www.w3.org/2000/svg"
   width="512.000000pt"
