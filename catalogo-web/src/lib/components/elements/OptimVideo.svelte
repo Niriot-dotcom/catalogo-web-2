cf2fc20592baf349c62d64c4153051dfedd6287b
Paty Lopez <lopezpatyyarely@gmail.com>
2024-10-07 09:09:59 -0600

    OptimVideo component.

---
+++ catalogo-web/src/lib/components/elements/OptimVideo.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let path: string;
+  export let style: string = "";
+</script>
+
+<!-- MOBILE -->
+<video
+  autoplay
+  loop
+  muted
+  playsinline
+  preload="none"
+  poster="{path}-VERTICAL.jpg"
+  class="md:hidden block {$$props.class}"
+  {style}
+>
+  <source src="{path}-VERTICAL.mp4" type="video/mp4" />
+  <source src="{path}-VERTICAL.webm" type="video/webm" />
+</video>
+
+<!-- DESKTOP -->
+<video
+  autoplay
+  loop
+  muted
+  playsinline
+  preload="none"
+  poster="{path}-HORIZONTAL.jpg"
+  class="hidden md:block {$$props.class}"
+  {style}
+>
+  <source src="{path}-HORIZONTAL.mp4" type="video/mp4" />
+  <source src="{path}-HORIZONTAL.webm" type="video/webm" />
+</video>
