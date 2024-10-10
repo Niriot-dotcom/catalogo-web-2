f617b44be689c9500b607d33d8c91e2b148c6f93
Paty Lopez <lopezpatyyarely@gmail.com>
2024-10-10 17:16:14 -0600

    Desktop version of InicioBiasi.

--- catalogo-web/src/lib/templates/biasi/InicioBiasi.svelte
+++ catalogo-web/src/lib/templates/biasi/InicioBiasi.svelte
@@ -1,5 +1,70 @@
+<script lang="ts">
+  let sectionsRefs = [
+    {
+      link: "/biasi/muebles",
+      image: "/images/biasi/indice/INDICE-AMUEBLANDO",
+      imgClass: "absolute bottom-5 left-12",
+      svgClass: "absolute -bottom-5 -right-12",
+    },
+    {
+      link: "/biasi/decoracion",
+      image: "/images/biasi/indice/INDICE-DECORANDO",
+      imgClass: "absolute top-5 left-0",
+      svgClass: "absolute top-7 right-5",
+    },
+    {
+      link: "/biasi/cocina",
+      image: "/images/biasi/indice/INDICE-COCINANDO",
+      imgClass: "absolute top-0 right-8",
+      svgClass: "absolute top-5 right-5",
+    },
+    {
+      link: "/biasi/bano",
+      image: "/images/biasi/indice/INDICE-BANO",
+      imgClass: "absolute -top-2 right-16",
+      svgClass: "absolute -bottom-5 -right-12",
+    },
+  ];
+</script>
+
 <!-- TODO animations not working -->
-<section id="inicio" class="introducci-n mt-16">
+
+<!-- DESKTOP -->
+<section class="hidden lg:block w-screen h-screen relative">
+  <div class="background-image bg-center"></div>
+
+  <div
+    class="absolute top-0 left-0 w-full h-full px-10 grid grid-cols-2 grid-rows-2"
+  >
+    {#each sectionsRefs as ref}
+      <div class="relative">
+        <a href={ref.link}>
+          <div class={ref.imgClass}>
+            <img
+              src="{ref.image}.webp"
+              loading="eager"
+              data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
+              alt=""
+              class="w-[24vw]"
+            />
+            <div class={ref.svgClass}>
+              <img
+                src="{ref.image}.svg"
+                loading="eager"
+                alt=""
+                data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
+                class="h-[7vw]"
+              />
+            </div>
+          </div>
+        </a>
+      </div>
+    {/each}
+  </div>
+</section>
+
+<!-- MOBILE -->
+<section id="inicio" class="lg:hidden introducci-n mt-16 lg:mt-0">
   <div class="div-block h-screen">
     <!-- <section class="categorias">
       <div class="w-full flex xs:p-16 sm:p-20 md:p-20 lg:p-40 p-10">
@@ -12,127 +77,6 @@
       </div>
     </section> -->
 
-    <!-- DESKTOP -->
-    <div class="hidden lg:block sticky-container primary-container">
-      <div class="sticky-parent primary-sticky">
-        <div class="card-parent pillows-background">
-          <section class="hero-image-left">
-            <div class="background-image bg-center"></div>
-
-            <div
-              class="absolute w-11/12 h-[70%] m-10 grid grid-cols-2 grid-rows-2"
-            >
-              <div class="relative">
-                <a href="/biasi/muebles">
-                  <div class="absolute bottom-10 left-24">
-                    <img
-                      src="/images/biasi/indice/INDICE-AMUEBLANDO.webp"
-                      loading="eager"
-                      data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
-                      alt=""
-                      class="w-[27vw]"
-                    />
-                    <div class="absolute -bottom-5 -right-12">
-                      <img
-                        src="/images/biasi/indice/INDICE-AMUEBLANDO.svg"
-                        loading="eager"
-                        alt=""
-                        data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
-                        class="h-[7vw]"
-                      />
-                    </div>
-                  </div>
-                </a>
-              </div>
-
-              <div class="relative">
-                <a href="/biasi/decoracion">
-                  <div class="absolute top-0 left-0">
-                    <img
-                      src="/images/biasi/indice/INDICE-DECORANDO.webp"
-                      loading="eager"
-                      data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
-                      alt=""
-                      class="w-[27vw]"
-                    />
-                    <div class="absolute top-7 right-5">
-                      <img
-                        src="/images/biasi/indice/INDICE-DECORANDO.svg"
-                        loading="eager"
-                        alt=""
-                        data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
-                        class="h-[7vw]"
-                      />
-                    </div>
-                  </div>
-                </a>
-              </div>
-
-              <div class="relative">
-                <a href="/biasi/cocina">
-                  <div class="absolute top-0 right-0">
-                    <img
-                      src="/images/biasi/indice/INDICE-COCINANDO.webp"
-                      loading="eager"
-                      data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
-                      alt=""
-                      class="w-[27vw]"
-                    />
-                    <div class="absolute top-5 right-5">
-                      <img
-                        src="/images/biasi/indice/INDICE-COCINANDO.svg"
-                        loading="eager"
-                        alt=""
-                        data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
-                        class="h-[7vw]"
-                      />
-                    </div>
-                  </div>
-                </a>
-              </div>
-
-              <div class="relative">
-                <a href="/biasi/bano">
-                  <div class="absolute -top-28 right-20">
-                    <img
-                      src="/images/biasi/indice/INDICE-BANO.webp"
-                      loading="eager"
-                      data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
-                      alt=""
-                      class="w-[27vw]"
-                    />
-                    <div class="absolute -bottom-5 -right-12">
-                      <img
-                        src="/images/biasi/indice/INDICE-BANO.svg"
-                        loading="eager"
-                        alt=""
-                        data-w-id="03369f7f-7877-3bc6-15b0-a390c7f48f9e"
-                        class="h-[7vw]"
-                      />
-                    </div>
-                  </div>
-                </a>
-              </div>
-
-              <div class="absolute bottom-0 w-full h-[10%]">
-                <div class="w-screen px-40">
-                  <div class="w-full flex xs:p-16 sm:p-20 md:p-20 lg:p-40 p-10">
-                    <div class="flex flex-col font-helvetica items-end w-full">
-                      <span class="text-5xl uppercase">Y MUCHO MÁS</span>
-                      <span class="text-5xl font-bold font-kepler italic"
-                        >no te lo pierdas</span
-                      >
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-      </div>
-    </div>
-
-    <!-- MOBILE -->
     <div class="lg:hidden sticky-container primary-container h-screen static">
       <div class="sticky-parent primary-sticky h-screen static">
         <div class="card-parent pillows-background h-screen">
--- catalogo-web/src/routes/+page.svelte
+++ catalogo-web/src/routes/+page.svelte
@@ -263,40 +263,6 @@
             </a>
           </div>
         </div>
-        <div class="min-w-0 sm:flex-1 md:px-8 lg:px-0 xl:col-span-8">
-          <div
-            class="flex items-center px-0 sm:px-6 py-2 sm:py-4 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0"
-          >
-            <div class="w-full">
-              <label for="search" class="sr-only">Search</label>
-              <div class="relative">
-                <div
-                  class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
-                >
-                  <svg
-                    class="h-5 w-5 text-gray-400"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </div>
-                <!-- <button
-                  on:click={handleOpenSearchPallete}
-                  id="search"
-                  class="block sm:w-full w-10 min-h-[44px] sm:min-h-0 cursor-pointer text-left truncate rounded-xl border-0 bg-white py-1.5 pl-10 pr-1 sm:pr-3 ring-1 ring-inset ring-white sm:ring-gray-300 text-gray-400 text-base sm:text-sm sm:leading-6 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-vianney-500 focus:bg-gray-100"
-                >
-                  <span class="hidden sm:block">Buscar</span>
-                </button> -->
-              </div>
-            </div>
-          </div>
-        </div>
 
         <div
           class="hidden lg:flex lg:items-center lg:justify-end xl:col-span-2"
